4700f3057b8fc613b539a6f886ed82b897aac14e
Jose Martin Ipong <joseipong@gmail.com>
2026-05-02 21:55:18 +0800

    Initial Commit

---
+++ Kanji Study/Kanji_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kanji_StudyApp.swift
+//  Kanji Study
+//
+//  Created by Martin on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Kanji_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kanji StudyTests/Kanji_StudyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kanji_StudyTests.swift
+//  Kanji StudyTests
+//
+//  Created by Martin on 5/2/26.
+//
+
+import Testing
+@testable import Kanji_Study
+
+struct Kanji_StudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
